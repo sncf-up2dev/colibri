73b8dc683d0c992068d91363b5ea2a2d283585b2
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-06-03 11:31:37 +0200

    Rename ErrorHandlerAdvice

--- colibri-app/src/main/java/fr/sncf/d2d/colibri/rest/common/ErrorHandlerAdvice.java
+++ colibri-app/src/main/java/fr/sncf/d2d/colibri/rest/common/ErrorHandlerAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ErrorHandlerConfig {
+public class ErrorHandlerAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
